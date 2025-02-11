c29c59e5277daee0ff97e9b740aa3c6dbf66be1f
David Kotlirevsky <david.kotlirevsky@gmail.com>
2025-02-10 21:27:43 -0300

    wip

--- src/main.js
+++ src/main.js
@@ -1,8 +1,12 @@
 function Experimento() {
-    SpreadsheetApp.getActiveSpreadsheet().toast('Experimento mesage', 'Aviso', 3);
+    SpreadsheetApp.getActiveSpreadsheet().getActiveCell().setValue("Exito")
+    SpreadsheetApp.getActiveSpreadsheet().toast('Experimento message fixed', 'Aviso', 3);
 }
 
 
 function Experimento2() {
     SpreadsheetApp.getActiveSpreadsheet().toast('Experimento mesage', 'Aviso', 3);
 }
+
+globalThis.GlobalFunctions.Experimento = Experimento
+globalThis.GlobalFunctions.Experimento2 = Experimento2
